c4c0e54d51266efef943db28d3eb102bb1ed391e
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-09-07 23:31:50 -0600

    Update listmQuoteInitial.js

--- public/js/ui_listmquote/listmQuoteInitial.js
+++ public/js/ui_listmquote/listmQuoteInitial.js
@@ -13,81 +13,35 @@ $(document).ready(function(){
     /////// tabla mquotes, estado inicial con todas las mquotes en el modulo
     //////////////////////////////////////////////////////////////////////////
 
-
-    /*var MyDateField = function(config) {
-        jsGrid.Field.call(this, config);
-    };
-
-    MyDateField.prototype = new jsGrid.Field({
-
-        css: "date-field",            // redefine general property 'css'
-        align: "center",              // redefine general property 'align'
-
-        myCustomProperty: "foo",      // custom property
-
-        sorter: function(date1, date2) {
-            return "new Date(date1) - new Date(date2)";
-        },
-
-        itemTemplate: function(value) {
-            return this._insertPicker = $("<input type='checkbox' class='form-control'>");
-        },
-
-        insertTemplate: function(value) {
-            return this._insertPicker = $("<input>");
-        },
-
-        editTemplate: function(value) {
-            return this._editPicker = $("<input>");
-        },
-
-        insertValue: function() {
-            return this._insertPicker;
-        },
-
-        editValue: function() {
-            return this._editPicker;
-        }
-    });
-
-    jsGrid.fields.date = MyDateField;*/
-
-
-    storeQuote.subscribe(() => {
+    /*storeQuote.subscribe(() => {
         dataQuotes = storeQuote.getState().quotes
         data = []
-
         if (_.isEmpty(dataQuotes)) {
             dataQuotes = {id: 1, Name:"Quote Test"}
         }
-
-
             $.map(dataQuotes, function(v) {
                 data.push(v)
-
             })
-
             $("#table-quotes").jsGrid({
                 width: "100%",
                 //height: "400px",
-
                 /*inserting: true,
-                editing: true,*/
+                editing: true,*
                 sorting: true,
                 paging: true,
                 pageSize: 10,
-
                 data: data,
                 fields: [
                     { type: "control", width: 10, title:"Options", editButton: false, deleteButton: false,
-                    itemTemplate: function(value, item) {
-<<<<<<< HEAD
-                        var $iconPencil = $(`<a><span class="material-icons edit" data-id="${item.id}">create</span></a>`);
-                        var $iconTrash = $(`<a><span class="material-icons delete" data-id=${item.id}>clear</span></a>`);
-=======
+                    itemTemplate: function(value, item)
+                        var $iconPencil = $(`<a><span class="material-icons oculto edit" data-id="${item.id}">create</span></a>`);
+                        var $iconTrash = $(`<a><span class="material-icons oculto delete" data-id=${item.id}>clear</span></a>`
                         var $iconPencil = $(`<a><span class="material-icons oculto edit" data-id="${item.id}">create</span></a>`);
                         var $iconTrash = $(`<a><span class="material-icons oculto delete" data-id=${item.id}>clear</span></a>`);
->>>>>>> parent of 3703978 (Merge branch 'esteban' of https://github.com/eulloa1982/zoho_crm_magaya_ext into esteban)
+                        var $iconPencil = $(`<a><i class="fa fa-edit fa-2x edit" data-id=${item.id}></i></a>`);
+                        var $iconTrash = $(`<a><i class="fa fa-times fa-2x delete" aria-hidden="true" data-id=${item.id}></i></a>`);
+                        var $iconPencil = $(`<a><i class="fa fa-edit fa-2x edit" data-id=${item.id}></i></a>`);
+                        var $iconTrash = $(`<a><i class="fa fa-times fa-2x delete" aria-hidden="true" data-id=${item.id}></i></a>`);
                         var $other = $(`<a><input type="checkbox" class="add" id="data-show" data-id=${item.id}/></a>`);
                         //var $other = $(`<a class="add">Show</a>`);
                         return $("<div>").attr({class: "btn-toolbar"})
@@ -95,39 +49,18 @@ $(document).ready(function(){
                             .append($iconPencil)
                             .append($iconTrash);
                     } },
-
                     //{ type: "checkbox",width: 5 },
                     { name: "Name", title: "NUMBER", width: 10, formatter:'number'},
                     { name: "Account.name", title: "CUSTOMER", type: "text", width: 10 },
                     { name: "magaya__Status", type: "text", width: 10, title: "STAGE" },
-                    { name: "magaya__Quote_Amount", title: "Amount", width: 10 },
+                    { name: "magaya__Amount", title: "AMOUNT", width: 10, type: "number" },
                     { name: "Modified_Time", type: "text", width: 10, title: "MODIFIED TIME" },
                     //{ name: "magaya__Description", type: "text", width: 30, title: "Description"},
                     //{ name: "magaya__ExpirationDate", type: "date", width: 60, title: "Expiration Date" },
                     //{ name: "magaya__Direction", type: "text", width: 15, title: "Direction"},
                 ]
-
             });
-    })
-
-    /*function sanitizer (cellvalue, options, rowObject) {
-        alert ("ddd")
-        if (cellvalue === "ASA500" || cellvalue == 1451520000) {
-            // a specific date that should show as blank
-            return '';
-        } else {
-            // here is where I'd like to just call the $.fmatter.util.DateFormat
-            var dt = new Date(cellvalue * 1000);
-            var op = $.extend({},opts.date);
-            if(!isUndefined(opts.colModel.formatoptions)) {
-                op = $.extend({},op,opts.colModel.formatoptions);
-            }
-            return $.fmatter.util.DateFormat(op.srcformat,dt,op.newformat,op);
-        }
-
-        //return cellvalue.replace(/<(|\/|[^>\/bi]|\/[^>bi]|[^\/>][^>]+|\/[^>][^>]+)>/g, '');
-    }*/
-
+    })*/
 
 
     ZOHO.embeddedApp.on("PageLoad",function(data)
@@ -162,6 +95,7 @@ $(document).ready(function(){
             .then(function(data){
                 $("#select-package").empty();
                 $.map (data.data, function (k, i){
+                    k.Name = sanitize(k.Name)
                     $("<option value='"+i+"'>"+k.Name+"</option>").appendTo("#select-package");
                     packageType.push(k);
                 })
@@ -181,9 +115,10 @@ $(document).ready(function(){
             .then(function(response){
                 $.map (response.data, function (k, i) {
                     var accountId = k.id;
-                    $("<option value='"+k.id+"'>"+k.Account_Name+"</option>").appendTo("select[name=magaya__Account]");
-                    $("<option value='"+k.id+"'>"+k.Account_Name+"</option>").appendTo("select[name=ShipperName]");
-                    $("<option value='"+k.id+"'>"+k.Account_Name+"</option>").appendTo("select[name=ConsigneeName]");
+                    k.Account_Name = sanitize(k.Account_Name)
+                    $("<option value='"+k.id+"'>"+k.Account_Name+"</option>").appendTo("select[name=Account]");
+                    $("<option value='"+k.id+"'>"+k.Account_Name+"</option>").appendTo("select[name=magaya__Shipper]");
+                    $("<option value='"+k.id+"'>"+k.Account_Name+"</option>").appendTo("select[name=magaya__ConsigneeName]");
                 })
                 storeAccounts.dispatch(addAccount(response.data))
             })
@@ -197,6 +132,7 @@ $(document).ready(function(){
         ZOHO.CRM.API.getAllRecords({Entity: "Deals", sort_order: "asc"})
             .then(function(response){
                 $.map (response.data, function (k, i) {
+                    k.Deal_Name = sanitize(k.Deal_Name)
                     $("<option value='"+k.id+"'>"+k.Deal_Name+"</option>").appendTo("select[name=Deals]");
                 })
             })
@@ -205,6 +141,7 @@ $(document).ready(function(){
         ZOHO.CRM.API.getAllRecords({Entity: "magaya__TransportationMethods", sort_order: "asc"})
             .then(function(response){
                 $.map (response.data, function (k, i) {
+                    k.Name = sanitize(k.Name)
                     $("<option value='"+k.id+"'>"+k.Name+"</option>").appendTo("select[name=magaya__TransportationMode]");
                     transpMethods.push (k);
                 })
@@ -215,17 +152,22 @@ $(document).ready(function(){
             .then(function (response) {
                 if (!_.isEmpty (response.data)) {
                     $.map(response.data, function (k, i) {
-                            $(`<option value="${k.magaya__ChargesCode}">${k.Name}</option>`).appendTo("select[name=ChargeType]");
+                        k.magaya__ChargesCode = sanitize(k.magaya__ChargesCode)
+                        k.Name = sanitize(k.Name)
+                        $(`<option value="${k.magaya__ChargesCode}">${k.Name}</option>`).appendTo("select[name=ChargeType]");
 
                     })
                 }
             })
 
         //Charges Type
-        ZOHO.CRM.API.getAllRecords({Entity: "magaya__ii", sort_order: "asc"})
+        ZOHO.CRM.API.getAllRecords({Entity: "magaya__Taxes", sort_order: "asc"})
             .then(function (response) {
                 if (!_.isEmpty (response.data)) {
+                    console.log("Taxes", response.data)
                     $.map(response.data, function (k, i) {
+                        k.magaya__Tax_Rate = sanitize(k.magaya__Tax_Rate)
+                        k.Name = sanitize(k.Name)
                         $(`<option value="${k.magaya__Tax_Rate}">${k.Name}</option>`).appendTo("select[name=TaxCode]");
 
                     })
