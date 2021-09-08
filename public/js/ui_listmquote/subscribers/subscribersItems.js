addcac1a880551dac9fe24df20a48ff6d2655efb
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-09-07 23:53:45 -0600

    Update subscribersItems.js

--- public/js/ui_listmquote/subscribers/subscribersItems.js
+++ public/js/ui_listmquote/subscribers/subscribersItems.js
@@ -1,63 +1,100 @@
+//si es true el module es table-items-new, sino es table-items
+let data_module_flag_item = true
+
 //get an item, draw the data view
 storeItem.subscribe(() => {
     let u = storeItem.getState().singleItem;
 
-    //construir los campos y la data
-    let append = ''
-    let id = 0;
-    //find id charge
-    $.map(u[1], function(k, v) {
-        id = u[1].id
-    })
-    /*if (id === undefined || id === 'undefined' || id === null) {
-        id = u[0]
-    }*/
-    $("#info-datad").empty()
-    $.map(u[1], function(k, v) {
-        if ( _.has(ITEMS_FIELDS, v)) {
-            let field = _.get(ITEMS_FIELDS, [v, 'field'])
-            append += `<div class="row" style="margin: 5px 5px 5px 5px">
-            <div data-id="${id}" class="col-md-6" style="font-weight: bold; padding: 5px 5px 5px 5px">${field}</div>
-            <div data-id="${id}" class="col-md-6" style="font-weight: bold; padding: 5px 5px 5px 5px"><input type="text" class="form-control no-border" name="${v}" value="${k}"/></div>
-            </div>`
-        }
-    })
+    if (!_.isEmpty(u)) {
+        //construir los campos y la data
+        let k = u[0]
+        let id = 0;
+        //find id charge
+        $.map(u[1], function(k, v) {
+            id = u[1].id
+        })
+        let data_module = data_module_flag_item ? "table-items-new" : "table-items"
+        let no_border = data_module_flag_item ? "no-border-item-new" : "no-border-item"
 
-    append += `<div class='close'>Close</div>`
-    $("#info-datad").append(append)
+        $("#info-datad").empty()
+        let append = `
+            <span class="material-icons cursor-hand btn-slide" data-module="${data_module}" data-id="${parseInt(k)-1}">arrow_back_ios_new</span>
+            <span class="material-icons cursor-hand btn-slide" data-module="${data_module}" data-id="${parseInt(k)+1}">arrow_forward_ios</span>
+        `
+        $.map(u[1], function(k, v) {
+            if ( _.has(ITEMS_FIELDS, v)) {
+                input = `<input type="text" data-id="${id}" class="form-control ${no_border}" name="${v}" value="${k}"/>`
+                let field = _.get(ITEMS_FIELDS, [v, 'field'])
+                let values = _.has(ITEMS_FIELDS, [v, "values"]) ? _.get(ITEMS_FIELDS, [v, 'values']) : ''
+                if (!_.isEmpty(values)) {
+                    input = `<select data-id="${id}" name="${v}" class="form-control no-border-item">`
+                        $.map(values, function(val) {
+                            if (val === k)
+                                input += `<option value="${val}" selected>${val}</option>`
+                            else
+                                input += `<option value="${val}" selected>${val}</option>`
+                        })
+                    input += `</select>`
+                }
+                append += `<div class="row" style="margin: 5px 5px 5px 5px">
+                <div class="col-md-6" style="font-weight: bold; padding: 5px 5px 5px 5px">${field}</div>
+                <div data-id="${id}" class="col-md-6" style="font-weight: bold; padding: 5px 5px 5px 5px">${input}</div>
+                </div>`
+            }
+        })
+
+        $("#info-datad").append(append)
+    }
 
 })
 
 //new items on table-items-new
 storeItem.subscribe(() => {
     let u = storeItem.getState().itemsOnNew;
-    $("#table-items-new tbody").empty();
+
     if (!_.isEmpty(u)) {
-        $.each(u, function(i, k) {
-            let measure_system = "in";
-            let measure_system_volume = "lb";
-            if (k.magaya__Measure_System === "International") {
-                measure_system = "m";
-                measure_system_volume = "kg";
-            }
+        data_module_flag_item = true;
+        $("#table-items-new tbody").empty();
+        if (!_.isEmpty(u)) {
+            let totalPieces = 0
+            let totalVolume = 0
+            let totalWeight = 0
+            $.each(u, function(i, k) {
+                let measure_system = "in";
+                let measure_system_volume = "lb";
+                if (k.magaya__Measure_System === "International") {
+                    measure_system = "m";
+                    measure_system_volume = "kg";
+                }
 
-            $("#table-items-new tbody").append(`<tr>
-            <td class='Delete'>
-                <span class="material-icons edit btn-slide" data-id="${i}">create</span>
-                <span class="material-icons delete del-item-warehouse-new" data-id=${i}>clear</span>
-            </td>
-            <td class="magaya__Pieces"><input type="text" class="form-control no-border" value="${k.magaya__Pieces}"/></td>
-            <td class='Name'><input type="text" class="form-control" value="${k.Name}" /></td>
-            <td class="magaya__Length"><input type="text" class="form-control" value="${roundDec(k.magaya__Length)}"/>(${measure_system})</td>
-            <td class="magaya__Height"><input type="text" class="form-control" value="${roundDec(k.magaya__Height)}"/>(${measure_system})</td>
-            <td class="magaya__Width"><input type="text" class="form-control" value="${roundDec(k.magaya__Width)}" />(${measure_system})</td>
-            <td class="magaya__Weigth"><input type="text" class="form-control" value="${roundDec(k.magaya__Weigth)}" />(${measure_system_volume})</td>
-            <td class="magaya__Volume"><input type="text" class="form-control" value="${roundDec(k.magaya__Volume)}"/></td>
-            <td class='magaya__Status' style="display: none;">InQuote</td>
-            <td class='magaya__Measure_System' style="display: none;">${k.magaya__Measure_System}</td>
-            </tr>`);
+                //get totales
+                totalPieces += parseInt(k.magaya__Pieces)
+                totalVolume += parseFloat(k.magaya__Volume)
+                totalWeight += parseFloat(k.magaya__Weigth)
 
-        })
+                $("#table-items-new tbody").append(`<tr>
+                <td class='Delete'>
+                    <span class="material-icons oculto btn-slide" data-id="${i}">create</span>
+                    <span class="material-icons oculto del-item-warehouse-new" data-id=${i}>clear</span>
+                </td>
+                <td class="magaya__Pieces"><input type="text" class="form-control no-border" value="${k.magaya__Pieces}"/></td>
+                <td class='Name'><input type="text" class="form-control" value="${sanitize(k.Name)}" /></td>
+                <td class="magaya__Length"><input type="text" class="form-control" value="${roundDec(k.magaya__Length)}"/>(${measure_system})</td>
+                <td class="magaya__Height"><input type="text" class="form-control" value="${roundDec(k.magaya__Height)}"/>(${measure_system})</td>
+                <td class="magaya__Width"><input type="text" class="form-control" value="${roundDec(k.magaya__Width)}" />(${measure_system})</td>
+                <td class="magaya__Weigth"><input type="text" class="form-control" value="${roundDec(k.magaya__Weigth)}" />(${measure_system_volume})</td>
+                <td class="magaya__Volume"><input type="text" class="form-control" value="${roundDec(k.magaya__Volume)}"/></td>
+                <td class='magaya__Status' style="display: none;">InQuote</td>
+                <td class='magaya__Measure_System' style="display: none;">${k.magaya__Measure_System}</td>
+                </tr>`);
+
+            })
+
+            $("input[name=Total_Pieces]").val(`${totalPieces}`)
+            $("input[name=Total_Weight]").val(`${totalWeight}`)
+            $("input[name=Total_Volume]").val(`${totalVolume}`)
+
+        }
     }
 })
 
@@ -65,35 +102,50 @@ storeItem.subscribe(() => {
 //////subscriber items, render UI table
 storeItem.subscribe(() => {
     let u = storeItem.getState().items;
-    $("#table-items tbody").empty();
-    $.each(u, function(i, k) {
-        let volume = parseFloat(k.magaya__Length) * parseFloat(k.magaya__Height) * parseFloat(k.magaya__Weigth);
-        let quote = k.magaya__SQuote_Name.id;
 
-        let measure_system = "in";
-        let measure_system_volume = "lb";
-        if (k.magaya__Measure_System === "International") {
-            measure_system = "m";
-            measure_system_volume = "kg";
-        }
+    if (!_.isEmpty(u)) {
+        data_module_flag_item = false;
+        let totalPieces = 0
+        let totalVolume = 0
+        let totalWeight = 0
 
-        let appendData = `<tr>
-        <td class="Delete">
-            <span class="material-icons edit btn-slide" data-id="${i}">create</span>
-            <span class="material-icons del-item-warehouse-new" data-id=${i}>clear</span>
-        </td>
-        <td><input type="text" class="form-control no-border" name="magaya__Pieces" value="${k.magaya__Pieces}"/></td>
-        <td class='Name'><input type="text" class="form-control no-border" name="Name" value="${k.Name}" /></td>
-        <td><input type="text" class="form-control no-border" name="magaya__Length" value="${k.magaya__Length}"/>(${measure_system})</div></td>
-        <td><input type="text" class="form-control no-border" name="magaya__Height" value="${k.magaya__Height}"/><div style="display:block;">(${measure_system})</div></td>
-        <td><input type="text" class="form-control no-border" name="magaya__Width" value="${k.magaya__Width}" /><div style="display:inline-block;">(${measure_system})</div></td>
-        <td><input type="text" class="form-control no-border" name="magaya__Weigth" value="${k.magaya__Weigth}" /><div style="display:inline;">(${measure_system_volume})</div></td>
-        <td class="magaya__Volume">${k.magaya__Volume}</td>
-        <td class="magaya__Status" style="display: none;">InQuote</td>
-        <td class='magaya__Measure_System' style="display: none;">${k.magaya__Measure_System}</td>
-        </tr>`
-        $("#table-items tbody").append(appendData);
+        $("#table-items tbody").empty();
+        $.each(u, function(i, k) {
+            let volume = parseFloat(k.magaya__Length) * parseFloat(k.magaya__Height) * parseFloat(k.magaya__Weigth);
+            let quote = k.magaya__SQuote_Name.id;
 
-    }) //each
+            let measure_system = "in";
+            let measure_system_volume = "lb";
+            if (k.magaya__Measure_System === "International") {
+                measure_system = "m";
+                measure_system_volume = "kg";
+            }
 
+            totalPieces += parseInt(k.magaya__Pieces)
+            totalVolume += parseFloat(k.magaya__Volume)
+            totalWeight += parseFloat(k.magaya__Weigth)
+
+            let appendData = `<tr>
+            <td class="Delete">
+                <span class="material-icons oculto btn-slide" data-id="${i}">create</span>
+                <span class="material-icons oculto del-item-warehouse" data-id=${k.id}>clear</span>
+            </td>
+            <td><input type="text" class="form-control no-border" name="magaya__Pieces" value="${k.magaya__Pieces}"/></td>
+            <td class='Name'><input type="text" class="form-control no-border" name="Name" value="${sanitize(k.Name)}" /></td>
+            <td><input type="text" class="form-control no-border" name="magaya__Length" value="${k.magaya__Length}"/>(${measure_system})</div></td>
+            <td><input type="text" class="form-control no-border" name="magaya__Height" value="${k.magaya__Height}"/><div style="display:block;">(${measure_system})</div></td>
+            <td><input type="text" class="form-control no-border" name="magaya__Width" value="${k.magaya__Width}" /><div style="display:inline-block;">(${measure_system})</div></td>
+            <td><input type="text" class="form-control no-border" name="magaya__Weigth" value="${k.magaya__Weigth}" /><div style="display:inline;">(${measure_system_volume})</div></td>
+            <td class="magaya__Volume">${k.magaya__Volume}</td>
+            <td class="magaya__Status" style="display: none;">InQuote</td>
+            <td class='magaya__Measure_System' style="display: none;">${k.magaya__Measure_System}</td>
+            </tr>`
+            $("#table-items tbody").append(appendData);
+
+        }) //each
+
+        $("input[name=Total_Pieces]").val(`${totalPieces}`)
+        $("input[name=Total_Weight]").val(`${totalWeight}`)
+        $("input[name=Total_Volume]").val(`${totalVolume}`)
+    }
 })
