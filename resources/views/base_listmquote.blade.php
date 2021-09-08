ca56191b96cae3937806319455028f52a50316a3
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-09-07 23:39:03 -0600

    base blade view

--- resources/views/base_listmquote.blade.php
+++ resources/views/base_listmquote.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css" />
     <!--link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css" /-->
@@ -19,9 +19,8 @@
     <link rel="stylesheet" href="{{ url('css/preloader.css') }}"/>
     <!--link rel="stylesheet" href="{{ url('css/dataTables/dataTables.bootstrap.css') }}"/-->
     <link rel="stylesheet" href="{{ url('css/table_listmquote.css') }}"/>
-    <!--link rel="stylesheet" href="{{ url('css/form_listmquote.css') }}"/>
-    <link rel="stylesheet" href="{{ url('css/font_css_material-design-iconic-font.min.css') }}"/-->
-    <link rel="stylesheet" href="{{ url('css/custom-lismquotes.css') }}"/>
+    <!--link rel="stylesheet" href="{{ url('css/form_listmquote.css') }}"/-->
+    <link rel="stylesheet" href="{{ url('css/font_css_material-design-iconic-font.min.css') }}"/>
 
 </head>
 <body style="margin-top:40px">
@@ -39,9 +38,15 @@
     </div>
 
 
-    <div id="quote-alert" class="alert alert-danger alert-dismissible" style="position: absolute; display: none; z-index: 6000"></div>
+    <div id="quote-alert" class="alert alert-danger alert-dismissible" style="position: absolute; display: none; z-index: 6000">
+        <button class="noselect"><span class='text'>Close</span><span id="close-all" data-close="quote-alert" class="icon">X</span></button>
+        <div id="message-alert"></div>
+
+    </div>
 
     <div id="quote-info" class="alert alert-success alert-dismissible" style="position: absolute; display: none; z-index: 6000;">
+        <button class="noselect"><span class='text'>Close</span><span id="close-all" data-close="quote-info" class="icon">X</span></button>
+        <div id="message-info"></div>
     </div>
 
 
@@ -73,25 +78,47 @@
     <script src="{{ url('js/magaya_api.js') }}"></script>
     <script src="{{ url('js/utils.js') }}"></script>
 
-    <script type="text/javascript" src="{{ url('js/ui_listmquote/listmQuoteInitial.js') }}" async="async"></script>
-    <script type="text/javascript" src="{{ url('js/ui_listmquote/listmQuote.js') }}" async="async"></script>
-    <script type="text/javascript" src="{{ url('js/ui_listmquote/listmQuoteButtons.js') }}" async="async"></script>
-
     <script src="{{ url('js/errors_handlers/errors.js') }}"></script>
     <script src="{{ url('js/store/constants.js') }}"></script>
     <script src="{{ url('js/store/storeError.js') }}"></script>
     <script src="{{ url('js/store/storeSuccess.js') }}"></script>
     <script src="{{ url('js/store/store.js') }}"></script>
     <script src="{{ url('js/store/storeAccounts.js') }}"></script>
-    <script src="{{ url('js/store/storeQuotes.js') }}" async="async"></script>
+    <script src="{{ url('js/store/storeQuotes.js') }}"></script>
     <script src="{{ url('js/store/storeItems.js') }}"></script>
     <script src="{{ url('js/store/storeCharges.js') }}"></script>
     <script src="{{ url('js/ui_listmquote/subscribers/subscribersCharges.js') }}"></script>
     <script src="{{ url('js/ui_listmquote/subscribers/subscribersItems.js') }}"></script>
+    <script src="{{ url('js/ui_listmquote/subscribers/subscribersQuotes.js') }}"></script>
+
+    <script type="text/javascript" src="{{ url('js/ui_listmquote/listmQuoteInitial.js') }}" async="async"></script>
+    <script type="text/javascript" src="{{ url('js/ui_listmquote/listmQuote.js') }}" async="async"></script>
+    <script type="text/javascript" src="{{ url('js/ui_listmquote/listmQuoteButtons.js') }}" async="async"></script>
+
 
     <!--script src="{{ url('js/store/storeForms.js') }}"></script-->
 
     @yield('js')
 
+
+    <div class="modal fade" id="exampleModal4" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel4" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-slideout modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal sideout large</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div id="info-datad"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
   </body>
 </html>
