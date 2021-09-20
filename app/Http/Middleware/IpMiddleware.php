527153835fcffde10d38220e29c407c93ebed3ff
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-09-19 23:33:01 -0600

    Probando middleware ip

---
+++ app/Http/Middleware/IpMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IpMiddleware
+{
+
+    public function handle($request, Closure $next)
+    {
+
+        if ($request->ip() != "204.141.42.49" && $request->ip() != "204.141.32.123") {
+        // here instead of checking a single ip address we can do collection of ips
+        //address in constant file and check with in_array function
+            //return redirect('home');
+            print_r($request->ip());
+        }
+
+        return $next($request);
+    }
+
+}
