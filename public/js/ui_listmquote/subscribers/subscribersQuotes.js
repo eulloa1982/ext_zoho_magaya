ee8521d708b14efd310781d1279947053e97c3da
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-09-25 10:14:12 -0600

    Update subscribersQuotes.js

--- public/js/ui_listmquote/subscribers/subscribersQuotes.js
+++ public/js/ui_listmquote/subscribers/subscribersQuotes.js
@@ -34,50 +34,51 @@ storeQuote.subscribe(() => {
     data = []
     if (_.isEmpty(dataQuotes)) {
         dataQuotes = {id: 1, Name:"Quote Test"}
-    }
+    } else {
 
-    $.map(dataQuotes, function(v) {
-        let mt =  v["Modified_Time"]
-        //primera porcion
-        mt = mt.split("T")
-        v["Modified_Time"] = `${mt[0]}`
-        if (mt[1]) {
-            mt[1] = mt[1].slice(0, -6)
-            v["Modified_Time"] += ` ${mt[1]}`
+        $.map(dataQuotes, function(v) {
+            let mt =  v["Modified_Time"]
+            //primera porcion
+            mt = mt.split("T")
+            v["Modified_Time"] = `${mt[0]}`
+            if (mt[1]) {
+                mt[1] = mt[1].slice(0, -6)
+                v["Modified_Time"] += ` ${mt[1]}`
+            }
+            data.push(v)
+
+        })
+
+        $("#table-quotes").jsGrid({
+            width: "100%",
+            sorting: true,
+            paging: true,
+            pageSize: 10,
+
+            data: data,
+            fields: [
+                { type: "control",  title:"Options", width: 'auto', editButton: false, deleteButton: false, title: "Action",
+                itemTemplate: function(value, item) {
+                    var $iconPencil = $(`<a><span class="material-icons oculto edit" data-id="${item.id}">create</span></a>`);
+                    var $iconTrash = $(`<a><span class="material-icons oculto delete" data-id="${item.id}">clear</span></a>`);
+                    var $sendMagaya = $(`<a><span class="material-icons oculto send" data-id="${item.id}">send</span></a>`);
+                    var $checkbox = $(`<a><input type="checkbox" class="quoteCheckBox" data-id="${item.id}" /></a>`);
+                    return $("<div>").attr({class: "btn-toolbar"})
+                                .append($checkbox)
+                                .append($sendMagaya)
+                                .append($iconPencil)
+                                .append($iconTrash);
+                    }
+                },
+
+                //{ type: "checkbox",width: 5 },
+                { name: "magaya__Number", title: "NUMBER", width: 220, formatter:'number'},
+                { name: "Account.name", title: "CUSTOMER", type: "text", width: 300},
+                { name: "magaya__Status", type: "text", width: 120, title: "STAGE" },
+                { name: "magaya__QuoteTotalAmount", title: "AMOUNT", width: 145, type: "number", formatter: "number", classes: 'number'},
+                { name: "Modified_Time", type: "text", width: 300, title: "MODIFIED TIME"},
+            ]
+
+            });
         }
-        data.push(v)
-
-    })
-
-    $("#table-quotes").jsGrid({
-        width: "100%",
-        sorting: true,
-        paging: true,
-        pageSize: 10,
-
-        data: data,
-        fields: [
-            { type: "control",  title:"Options", width: 'auto', editButton: false, deleteButton: false, title: "Action",
-            itemTemplate: function(value, item) {
-                var $iconPencil = $(`<a><span class="material-icons oculto edit" data-id="${item.id}">create</span></a>`);
-                var $iconTrash = $(`<a><span class="material-icons oculto delete" data-id="${item.id}">clear</span></a>`);
-                var $sendMagaya = $(`<a><span class="material-icons oculto send" data-id="${item.id}">send</span></a>`);
-                var $checkbox = $(`<a><input type="checkbox" class="quoteCheckBox" data-id="${item.id}" /></a>`);
-                return $("<div>").attr({class: "btn-toolbar"})
-                            .append($checkbox)
-                            .append($sendMagaya)
-                            .append($iconPencil)
-                            .append($iconTrash);
-                }
-            },
-
-            //{ type: "checkbox",width: 5 },
-            { name: "magaya__Number", title: "NUMBER", width: 220, formatter:'number'},
-            { name: "Account.name", title: "CUSTOMER", type: "text", width: 300},
-            { name: "magaya__Status", type: "text", width: 120, title: "STAGE" },
-            { name: "magaya__QuoteTotalAmount", title: "AMOUNT", width: 145, type: "number", formatter: "number", classes: 'number'},
-            { name: "Modified_Time", type: "text", width: 300, title: "MODIFIED TIME"},
-        ]
-
-        });
     })
