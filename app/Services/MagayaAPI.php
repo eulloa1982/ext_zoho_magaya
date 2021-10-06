10a0121a16406681287abf1e624b2ef55da79b8d
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-10-06 16:25:06 -0600

    Update MagayaAPI.php

--- app/Services/MagayaAPI.php
+++ app/Services/MagayaAPI.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Services;
-use SoapClient;
+use \SoapClient;
 
-class MagayaAPI extends SoapClient implements MagayaAPIInterface
+class MagayaAPI extends \SoapClient implements MagayaAPIInterface
 {
     const  ERROR_CODES = [
         'no_error'                    =>    "API call succeeded",
