57f47e1324b305fbeb10c72ab7ac34aca95075bf
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-10-11 20:32:14 -0600

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -41,6 +41,7 @@ class APIController extends Controller
         $access_key = $data[0];
         $contactData = $request->get('contactData');
         $emailCustomer = $contactData['Email'];
+        $typeEntity = $contactData['Type'];
         $contactData['Email'] = $randomEmail = $this->generateRandomEmail();
         //check magaya guid, if it null drop it from the array
         if ($contactData['Attribute_GUID'] == 0 || $contactData['Attribute_GUID'] == "null") {
@@ -52,15 +53,32 @@ class APIController extends Controller
         $toXml = new ArrayToXml();
         $toXml::$type = 'Client';
         $XML = $toXml::xml_from_array($contactData);
+
         $XML = str_replace('<?xml version="1.0"?>','',$XML);
         $XML = str_replace('<Client',"<Entity xmlns=\"http://www.magaya.com/XMLSchema/V1\"", $XML);
-        $XML = str_replace('</Client>','<Type>Client</Type></Entity>', $XML);
+        $XML = str_replace('</Client>', "</Entity>", $XML);
+
 
         $result = $this->magayaApi->SetEntity($access_key, 524288, $XML);
         $customer_data = '';
 
+        print_r($XML);
+        print_r($result);
+
         if (!$result['error'] || $result['error'] != 1) {
+            echo "No error";
             //find customer by email
+            //check the type of entity to change the method
+            /*if (strcasecmp("EntityContact", $typeEntity)) {
+                //metodo EntityContact
+
+            }
+            else if (strcasecmp("Client", $typeEntity)){
+                //metodo 2
+            }
+            else {
+                //no method
+            }*/
             $magaya_customers = $this->magayaApi->GetEntitiesOfType($access_key, 524288, '', 2);
             foreach ($magaya_customers['data'] as $key => $value) {
                 if ($value->Email == $randomEmail) {
@@ -81,14 +99,19 @@ class APIController extends Controller
             $XMLUpd = $toXmlUpd::xml_from_array($contactData);
             $XMLUpd = str_replace('<?xml version="1.0"?>','',$XMLUpd);
             $XMLUpd = str_replace('<Client',"<Entity xmlns=\"http://www.magaya.com/XMLSchema/V1\"", $XMLUpd);
-            $XMLUpd = str_replace('</Client>','<Type>Client</Type></Entity>', $XMLUpd);
+            $XMLUpd = str_replace('</Client>',"</Entity>", $XMLUpd);
+
+            //print_r($XMLUpd);
+            //return "fsdf45345435 345345";
             $result = $this->magayaApi->SetEntity($access_key, 524288, $XMLUpd);
             return $customer_guid;
 
         } else {
+            echo "Error";
             return false;
         }
     }
+
 
     /*
     *get working ports en Magaya
