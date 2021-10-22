6af8a1ce90b2cef67c088835e9ffbcdc01978267
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-10-21 20:43:32 -0600

    Update list_mquotes.blade.php

--- resources/views/tab_widget/list_mquotes.blade.php
+++ resources/views/tab_widget/list_mquotes.blade.php
@@ -58,7 +58,6 @@ a {
         <legend class="legend"><span>Search Results</span>
             <span class="material-icons close btn btn-danger float-right" style="margin: 0px 0px 0px 4px;color: white;background: none;border: none;" data-close="panel-search">close</span>
         </legend>
-s
         <div id="search">
 
             <table id="quote-search" class="table table-bordred table-striped">
