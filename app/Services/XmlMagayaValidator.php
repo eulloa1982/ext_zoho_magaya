df81dfa78536a5d750a07a4c712ece8c8c2a5d53
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-10-27 19:12:41 -0600

    Validaciones del GUID en contact y carrier XML

--- app/Services/XmlMagayaValidator.php
+++ app/Services/XmlMagayaValidator.php
@@ -11,7 +11,11 @@ class XmlMagayaValidator
         'no_status_item_quotation'    =>    "Items does not have a valid Status. ",
         'no_status_cargo_quotation'   =>    "Cargo does not have a valid Status. ",
         'no_quotation_type'           =>    "Quotation must be No Commerce Type",
-        'no_quotation_item_cargo'     =>    "Quotation must have Item or Charge"
+        'no_quotation_item_cargo'     =>    "Quotation must have Item or Charge",
+        'no_guid_consignee'           =>    "Consignee has no GUID Attribute",
+        'no_guid_contact'           =>    "Contact has no GUID Attribute",
+        'no_guid_shipper'           =>    "Shipper has no GUID Attribute",
+        'no_guid_carrier'           =>    "Carrier has no GUID Attribute"
         ];
 
     const TYPES_STATUS_QT = array('Open', 'Posted', 'Empty');
@@ -39,6 +43,29 @@ class XmlMagayaValidator
             $message['error'] = $this::ERROR_CODES['no_quotation_type'];
             return $message;
         }
+
+        if (empty($xml->Contact[0]['GUID']) || $xml->Contact[0]['GUID'] == null) {
+            $message['error'] = $this::ERROR_CODES['no_guid_contact'];
+            return $message;
+        }
+
+        if (!empty($xml->Carrier[0]['GUID']) && $xml->Carrier[0]['GUID'] == "null") {
+            $message['error'] = $this::ERROR_CODES['no_guid_carrier'];
+            return $message;
+        }
+
+        //Check Shipper and Consignee
+        /*if (empty($xml->Consignee[0]['GUID']) || $xml->Consignee[0]['GUID'] == null) {
+            $message['error'] = $this::ERROR_CODES['no_guid_consignee'];
+            return $message;
+        }
+
+        if (empty($xml->Shipper[0]['GUID']) || $xml->Shipper[0]['GUID'] == null) {
+            $message['error'] = $this::ERROR_CODES['no_shipper_consignee'];
+            return $message;
+        }*/
+
+
         /*if (!in_array ($xml->Status, $this::TYPES_STATUS_QT)) {
             $message['error'] = $this::ERROR_CODES['no_status_quotation'];
             return $message;
