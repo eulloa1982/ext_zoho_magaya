3e71d3d32013efef6296d0058f7951b1f691044d
brugo87 <83411488+brugo87@users.noreply.github.com>
2021-10-31 16:37:43 -0400

    comentariando lineas para prueba

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -115,8 +115,8 @@ $app->middleware([
 | can respond to, as well as the controllers that may handle them.
 |
 */
-$app->register(Barryvdh\DomPDF\ServiceProvider::class);
-$app->configure('dompdf');
+//$app->register(Barryvdh\DomPDF\ServiceProvider::class);
+//$app->configure('dompdf');
 
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
