630db886c1c20c281e2dac2c73e2e83ba496dd76
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-10-31 16:51:56 -0600

    Update quotation_pdf.blade.php

--- resources/views/tab_widget/quotation_pdf.blade.php
+++ resources/views/tab_widget/quotation_pdf.blade.php
@@ -1,74 +1,17 @@
 @extends('base_listmquote')
 @section('main')
-
-<?php
-//print_r($organization['orgData']['city']);
-
-/*
-<table><tr><td>Name</td></tr>
-@foreach ($organization as $dat)
-        <li>{{ $dat->city }}</li>
-
-        @endforeach
-
-
-</table>
-
-*/
-?>
-<div class="container">
-        <table cellspacing="0px" cellpadding="2px" style="border: none;" width="100%">
-            <tr>
-                <th rowspan="2">
-                    <table>
-                        <tr>
-                            <th>
-                                <div class="session-first" style="float: left; font-size: 28px;">
-                                    {{$organization['orgData']['company_name']}}
-                                </div>
-                            </th>
-                        </tr>
-                    </table>
-                </th>
-                <th>
-                    <table id="header" cellspacing="0px" cellpadding="2px" style="border: none; text-align: right;float: right;">
-                        <tr>
-                            <td colspan="12">
-                                <div class="col headerFont p-2"><span class="material-icons">
-                            language</span>{{$organization['orgData']['website']}}</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="12">
-                                <div class="col headerFont p-2"><span class="material-icons">
-                            phone</span>{{$organization['orgData']['phone']}}</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="12">
-                                <div class="col headerFont p-2"><span class="material-icons">
-                            alternate_email</span>{{$organization['orgData']['primary_email']}}</div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="12">
-                                <div class="col headerFont p-2"><span class="material-icons">
-                            home</span>{{$organization['orgData']['country']}}, {{$organization['orgData']['city']}}, {{$organization['orgData']['state']}}</div>
-                            </td>
-                        </tr>
-                    </table>
-                </th>
-            </tr>
-            <tr></tr>
-            <tr></tr>
-        </table>
-    </div>
+<style>
+th, td {
+  padding: 2px;
+}
+</style>
+<img src="{{ url('images/pdfv1/magaya.png') }}" width="60px" height="60px" alt="Quote" />
+    @include('tab_widget.partials.organization')
 
 <br /><br /><br /><br /><br /><br />
     @include('tab_widget.partials.mquote')
 
 <br /><br />
-<div class="container">
 
     <table width="100%" cellspacing="0px" cellpadding="0px">
         <tr>
@@ -84,10 +27,8 @@
         @include('tab_widget.partials.charges')
 
     </table>
-</div>
 
 <br /><br />
-<div class="container">
     <table width="100%">
         <tr>
             <th colspan="5" style="background-color: lightgrey; text-align: start;text-transform:uppercase ;border: 1px #000 solid;">Items</th>
@@ -102,11 +43,17 @@
         @include('tab_widget.partials.items')
 
     </table>
-</div>
 
+<br /><br />
 
+    <table width="100%">
+        <tr>
+            <th style="background-color: lightgrey; text-align: start;text-transform:uppercase ;border: 1px #000 solid;">Terms</th>
+        </tr>
 
+        @include('tab_widget.partials.terms')
 
+    </table>
 
 
 
