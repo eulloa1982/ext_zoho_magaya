f5f60220a0923cdd6abe69050028e53aa8cf2047
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-10-31 16:51:21 -0600

    Create terms.blade.php

---
+++ resources/views/tab_widget/partials/terms.blade.php
@@ -0,0 +1,7 @@
+<tr>
+    <td class="dataFont" style="text-align: start; padding: 3px;border: 1px #000 solid;">
+        @if (@isset($organization['mQuote']['magaya__Terms']))
+            {{$organization['mQuote']['magaya__Terms']}}
+        @endif
+    </td>
+</tr>
