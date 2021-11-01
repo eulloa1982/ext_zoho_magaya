c84b780bd1d595dc1550181e46047ede991cd6ec
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-11-01 15:19:08 -0600

    Charges totales

--- resources/views/tab_widget/partials/charges.blade.php
+++ resources/views/tab_widget/partials/charges.blade.php
@@ -1,11 +1,18 @@
 @if (@isset($organization['charges']))
+    {{$amount_total = 0;}}
     @foreach ($organization['charges'] as $dat=>$v)
+        {{ $amount_total += $v['magaya__Amount_Total'] }}
         <tr>
-            <td style="border-bottom: 1px #000 solid;border-left: 1px #000 solid;border-right: 1px #000 solid; border-bottom: 1px #000 solid; text-align: left;">{{$v['Name']}}</td>
+            <td style="border-bottom: 1px #000 solid;border-left: 1px #000 solid;border-right: 1px #000 solid; border-bottom: 1px #000 solid; text-align: center;">{{$v['Name']}}</td>
             <td style="border-bottom: 1px #000 solid;border-right: 1px #000 solid; border-bottom: 1px #000 solid; text-align: right;">{{$v['magaya__Price']}}</td>
             <td style="border-bottom: 1px #000 solid;border-right: 1px #000 solid; border-bottom: 1px #000 solid; text-align: right;">{{$v['magaya__CQuantity']}}</td>
-            <td style="border-right: 1px #000 solid; text-align: right;">{{$v['magaya__Amount']}}</td>
-            <td style="border-right: 1px #000 solid; text-align: right; font-weight: bold;">{{$v['magaya__Amount_Total']}}</td>
+            <td style="border-bottom: 1px #000 solid;border-right: 1px #000 solid; text-align: right;">{{$v['magaya__Amount']}}</td>
+            <td style="border-bottom: 1px #000 solid;border-right: 1px #000 solid; text-align: right; font-weight: bold;">{{$v['magaya__Amount_Total']}}</td>
         </tr>
     @endforeach
+    <tr>
+        <td colspan="3"></td>
+        <td style="border-left:1px #000 solid; border-bottom: 1px #000 solid;border-right: 1px #000 solid; border-bottom: 1px #000 solid; text-align: right;">TOTAL</td>
+        <td style="border-bottom: 1px #000 solid;border-right: 1px #000 solid; text-align: right; font-weight: bold;">{{$amount_total}}</td>
+    </tr>
 @endif
