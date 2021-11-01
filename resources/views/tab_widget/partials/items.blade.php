cabc32600decdbf8841d788a399f1a91421467d1
brugo87 <83411488+brugo87@users.noreply.github.com>
2021-10-31 22:17:58 -0400

    subiendo ultimos cambios

--- resources/views/tab_widget/partials/items.blade.php
+++ resources/views/tab_widget/partials/items.blade.php
@@ -3,16 +3,17 @@
     {{$total_volume = 0}}
     {{$measure_length = "in"}}
     {{$measure_weigth = "lb"}}
-    {{$measure_volume = "ft<sup>3</sup>"}}        
+    {{$measure_volume = "ft"}}        
     {{$total_weight_international = 0}}
     {{$total_volume_international = 0}}
     {{$total_weight_english = 0}}
     {{$total_volume_english = 0}}
-    
+    {{$measure_system = ''}}
+        
     @foreach ($organization['items'] as $dat=>$v)
         @if ($v['magaya__Measure_System'] === "International") {
             {{$measure_length = "m"}}
-            {{$measure_volume = "m<sup>3</sup>"}}
+            {{$measure_volume = "m"}}
             {{$measure_weigth = "kg"}}
             {{$total_volume_international += ($v['magaya__Volume'] * $v['magaya__Pieces'])}}
             {{$total_weight_international += ($v['magaya__Weigth'] * $v['magaya__Pieces'])}}
@@ -21,12 +22,13 @@
             {{$total_volume_english += ($v['magaya__Volume'] * $v['magaya__Pieces'])}}
             {{$total_weight_english += ($v['magaya__Weigth'] * $v['magaya__Pieces'])}}
         @endif
+        {{$measure_system = $v['magaya__Measure_System']}}
         <tr>
             <td width="40%" style="border-left: 1px #000 solid;border-right: 1px #000 solid;border-bottom: 1px #000 solid; text-align: center;">{{$v['magaya__Package_Type']['name']}}</td>
             <td width="15%" style="border-right: 1px #000 solid;border-bottom: 1px #000 solid; text-align: center;">{{$v['magaya__Pieces']}}</td>
             <td width="15%" style="border-right: 1px #000 solid;border-bottom: 1px #000 solid; text-align: center;">{{$v['magaya__Length']}} X {{$v['magaya__Width']}} X {{$v['magaya__Height']}} ({{$measure_length}})</td>
             <td width="15%" style="border-right: 1px #000 solid; text-align: right;border-bottom: 1px #000 solid;">{{$v['magaya__Weigth']}} ({{$measure_weigth}})</td>
-            <td width="15%" style="border-right: 1px #000 solid; text-align: right;border-bottom: 1px #000 solid;">{{$v['magaya__Volume']}} ({{$measure_volume}})</td>
+            <td width="15%" style="border-right: 1px #000 solid; text-align: right;border-bottom: 1px #000 solid;">{{$v['magaya__Volume']}} ({{$measure_volume}}<sup>3</sup>)</td>
         </tr>
     @endforeach
     {{$total_weight = ($total_weight_international + $total_weight_english) * 0.453562}}
@@ -35,6 +37,6 @@
         <td colspan="2"></td>
         <td style="border-left:1px #000 solid; border-bottom: 1px #000 solid;border-right: 1px #000 solid; border-bottom: 1px #000 solid; text-align: right;">TOTAL</td>
         <td style="border-bottom: 1px #000 solid;border-right: 1px #000 solid; text-align: right; font-weight: bold;">{{ $total_weight }} ({{$measure_weigth}})</td>
-        <td style="border-bottom: 1px #000 solid;border-right: 1px #000 solid; text-align: right; font-weight: bold;">{{ $total_volume }} ({{$measure_volume}})</td>
+        <td style="border-bottom: 1px #000 solid;border-right: 1px #000 solid; text-align: right; font-weight: bold;">{{ $total_volume }} ({{$measure_volume}}<sup>3</sup>)</td>
     </tr>
 @endif
