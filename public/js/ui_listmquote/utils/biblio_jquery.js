d66357c46fb5f6ff766a0442923ef5281e9dd465
brugo87 <83411488+brugo87@users.noreply.github.com>
2021-11-03 18:01:50 -0400

    testing pdf en el sandbox

--- public/js/ui_listmquote/utils/biblio_jquery.js
+++ public/js/ui_listmquote/utils/biblio_jquery.js
@@ -1009,7 +1009,7 @@ async function buildPdf(mquote_id) {
     }
 
     //const endpoint = `http://localhost/zoho_magaya/blog/public/pdf`;
-    const endpoint = `https://zohomagaya.herokuapp.com/pdf`
+    const endpoint = `https://zohomagayab.herokuapp.com/pdf`
 
     fetch(endpoint, {
         method: 'POST',
