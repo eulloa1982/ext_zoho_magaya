48a4298223b307ef1ca06472aa2a46cef07ac537
brugo87 <83411488+brugo87@users.noreply.github.com>
2021-11-03 17:39:48 -0400

    subiendo ultimos cambios del estilo del pdf

--- resources/views/tab_widget/partials/mquote.blade.php
+++ resources/views/tab_widget/partials/mquote.blade.php
@@ -1,113 +1,130 @@
 @if (@isset($organization['mQuote']))
-<div class="container" style="margin-top: 10px">
-    <table cellspacing="0px" cellpadding="0px" width="100%">
-        <tr>
-            <td width="40%">
-                <table id="info1" cellspacing="0px" cellpadding="0px" width="100%" style="text-align: left;">
-                    <tr>
-                        <th class="headerFont" colspan="2" style="border: 1px #000 solid; padding: 3px;text-align: center;">Customer Info</th>
-                    </tr>
-                    <tr>
-                        <th class="headerFont" style="text-align: right; padding: 3px; border-left: 1px #000 solid;border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
-                            Customer</th>
-                        <td class="dataFont" style="text-align: start; padding: 3px;border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
-                            @if (@isset($organization['mQuote']['Account']))
-                                {{$organization['mQuote']['Account']['name']}}
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="headerFont" style="text-align: right; padding: 3px; border-left: 1px #000 solid;border-right: 1px #000 solid;">
-                            Representative</th>
-                        <td class="dataFont" style="text-align: start; padding: 3px;border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
-                            @if (@isset($organization['mQuote']['magaya__Representative']))
-                                {{$organization['mQuote']['magaya__Representative']['name']}}
-                            @endif
+<div class="container" style="margin-top: 10px"></div>
+<table cellspacing="0px" cellpadding="0px" width="100%">
+    <tr>
+        <td width="50%">
+            <table id="info1" cellspacing="0px" cellpadding="3px" width="100%" style="text-align: left;">
+                <tr>
+                    <th class="headerFont" colspan="2" style="border: 1px #000 solid; padding: 3px;text-align: center;">Customer Info</th>
+                </tr>
+                <tr>
+                    <th class="headerFont" style="text-align: right; border-left: 1px #000 solid;border-right: 1px #000 solid;border-bottom: 1px #000 solid; font-size:14px">
+                        Customer</th>
+                    <td class="dataFont" style="text-align: start; border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
+                        @if (@isset($organization['mQuote']['Account']))
+                            {{$organization['mQuote']['Account']['name']}}
+                        @endif
+                    </td>
+                </tr>
+                <tr>
+                    <th class="headerFont" style="text-align: right; border-left: 1px #000 solid;border-right: 1px #000 solid;font-size:14px">
+                        Representative</th>
+                    <td class="dataFont" style="text-align: start; border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
+                        @if (@isset($organization['mQuote']['magaya__Representative']))
+                            {{$organization['mQuote']['magaya__Representative']['name']}}
+                        @endif
 
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="headerFont" style="text-align: right; padding: 3px; border: 1px #000 solid;">
-                            Phone</th>
-                        <td class="dataFont" style="text-align: start; padding: 3px;border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
-                            {{$organization['mQuote']['magaya__ContactMobile']}}</td>
-                    </tr>
-                    <tr>
-                        <th class="headerFont" style="text-align: right; padding: 3px;border-left: 1px #000 solid;border-right: 1px #000 solid;">
-                            Email</th>
-                        <td class="dataFont" style="text-align: start; padding: 3px;border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
-                            {{$organization['mQuote']['magaya__ContactEmail']}}</td>
-                    </tr>
-                    <tr>
-                        <th class="headerFont" style="text-align: right;border: 1px #000 solid;">
-                            Address</th>
-                        <td class="dataFont" style="text-align: start;border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
-                        </td>
-                    </tr>
-                </table>
+                    </td>
+                </tr>
+                <tr>
+                    <th class="headerFont" style="text-align: right; border: 1px #000 solid;font-size:14px">
+                        Phone</th>
+                    <td class="dataFont" style="text-align: start; padding: 3px;border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
+                        @if (@isset($organization['mQuote']['magaya__ContactMobile']))
+                        {{$organization['mQuote']['magaya__ContactMobile']}}
+                        @endif
+                    </td>
+                </tr>
+                <tr>
+                    <th class="headerFont" style="text-align: right; border-left: 1px #000 solid;border-right: 1px #000 solid;font-size:14px">
+                        Email</th>
+                    <td class="dataFont" style="text-align: start; border-right: 1px #000 solid;border-bottom: 1px #000 solid;">
+                        @if (@isset($organization['mQuote']['magaya__ContactEmail']))
+                        {{$organization['mQuote']['magaya__ContactEmail']}}
+                        @endif
+                    </td>
+                </tr>
+                <tr>
+                    <th class="headerFont" style="text-align: right;border: 1px #000 solid;font-size:14px">
+                        Address</th>
+                    <td class="dataFont" style="text-align: start;border-right: 1px #000 solid;border-bottom: 1px #000 solid;font-size:13px">
+                        @if (@isset($organization['mQuote']['magaya__BillingStreet']))
+                            {{$organization['mQuote']['magaya__BillingStreet']}}
+                        @endif
+                        @if (@isset($organization['mQuote']['magaya__BillingCity']))
+                            , {{$organization['mQuote']['magaya__BillingCity']}}
+                        @endif
+                        @if (@isset($organization['mQuote']['magaya__BillingState']))
+                            , {{$organization['mQuote']['magaya__BillingState']}}
+                        @endif
+                        @if (@isset($organization['mQuote']['magaya__BillingCountry']))
+                            , {{$organization['mQuote']['magaya__BillingCountry']}}
+                        @endif
+                    </td>
+                </tr>
+            </table>
 
-            </td>
-            <td width="60%">
-                <table id="info2" cellspacing="0px" cellpadding="0px" style="text-align:left;border: none; float: right;margin-left: 45px;">
-                    <tr>
-                        <th class="headerFont" style="text-align: right;">
-                            Quote Number:</th>
-                        <td class="dataFont">
-                            {{$organization['mQuote']['Name']}}</td>
-                    </tr>
-                    <tr>
-                        <th class="headerFont" style="text-align: right;">
-                            Creation Date:</th>
-                        <td class="dataFont">
-                            <?php echo explode('T',$organization['mQuote']['Created_Time'])[0]; ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="headerFont" style="text-align: right;">
-                            Expiration Date:</th>
-                        <td class="dataFont">
-                            <?php echo explode('T',$organization['mQuote']['magaya__ExpirationDate'])[0]; ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="headerFont" style="text-align: right;">
-                            Contact To:</th>
-                        <td class="dataFont">
-                            {{$organization['mQuote']['Owner']['name']}},<br> {{$organization['mQuote']['Owner']['email']}}
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <table width="100%" cellspacing="0px" cellpadding="0px" style="border: none; margin-top: 10px;">
-                    <tr>
-                        <th class="headerFont" colspan="2" style="background-color: lightblue; text-align: start;text-transform:uppercase ;border-left: 1px #000 solid;border-right: 1px #000 solid;border-top: 1px #000 solid;">
-                            Quotation Info
-                        </th>
-                    </tr>
-                    <tr>
-                        <th colspan="2" style="border-left: 1px #000 solid;border-right: 1px #000 solid;border-top: 1px #000 solid; text-align: start;">
-                            <div class="dataFont"><span class="headerFont">Description of Goods:</span>
-                            {{$organization['mQuote']['magaya__Description']}}</div>
-                        </th>
-                    </tr>
-                    <tr>
-                        <th style="border-left: 1px #000 solid;border-top: 1px #000 solid;border-right: 1px #000 solid;border-bottom: 1px #000 solid;text-align: start;">
-                            <div class="dataFont" style="text-align: start;"><span class="headerFont">Origin:</span>
-                            {{$organization['mQuote']['magaya__Origin']}}</div>
-                        </th>
-                        <th style="border-top: 1px #000 solid;border-right: 1px #000 solid;border-bottom: 1px #000 solid;text-align: start;">
-                            <div class="dataFont" style="text-align: start;"><span class="headerFont">Destination:</span>
-                            {{$organization['mQuote']['magaya__Destination']}}</div>
-                        </th>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-</div>
+        </td>
+        <td width="60%">
+            <table id="info2" cellspacing="0px" cellpadding="1px" style="text-align:left;border: none; float: right;margin-left: 45px;">
+                <tr>
+                    <th class="headerFont" style="text-align: right;font-size:14px">
+                        Quote Number:</th>
+                    <td class="dataFont" style="font-size:13px">
+                        {{$organization['mQuote']['Name']}}</td>
+                </tr>
+                <tr>
+                    <th class="headerFont" style="text-align: right;font-size:14px">
+                        Creation Date:</th>
+                    <td class="dataFont" style="font-size:13px">
+                        <?php echo explode('T',$organization['mQuote']['Created_Time'])[0]; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th class="headerFont" style="text-align: right;font-size:14px">
+                        Expiration Date:</th>
+                    <td class="dataFont" style="font-size:13px">
+                        <?php echo explode('T',$organization['mQuote']['magaya__ExpirationDate'])[0]; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th class="headerFont" style="text-align: right;font-size:14px">
+                        Contact To:</th>
+                    <td class="dataFont" style="font-size:13px">
+                        {{$organization['mQuote']['Owner']['name']}},<br> {{$organization['mQuote']['Owner']['email']}}
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2">
+            <table width="100%" cellspacing="0px" cellpadding="0px" style="border: none; margin-top: 10px;">
+                <tr>
+                    <th class="headerFont" colspan="2" style="background-color: lightblue; text-align: start;text-transform:uppercase ;border-left: 1px #000 solid;border-right: 1px #000 solid;border-top: 1px #000 solid;">
+                        Quotation Info
+                    </th>
+                </tr>
+                <tr>
+                    <th colspan="2" style="border-left: 1px #000 solid;border-right: 1px #000 solid;border-top: 1px #000 solid; text-align: start;">
+                        <div class="dataFont"><span class="headerFont">Description of Goods:</span>
+                        {{$organization['mQuote']['magaya__Description']}}</div>
+                    </th>
+                </tr>
+                <tr>
+                    <th style="border-left: 1px #000 solid;border-top: 1px #000 solid;border-right: 1px #000 solid;border-bottom: 1px #000 solid;text-align: start;">
+                        <div class="dataFont" style="text-align: start;"><span class="headerFont">Origin:</span>
+                        {{$organization['mQuote']['magaya__Origin']}}</div>
+                    </th>
+                    <th style="border-top: 1px #000 solid;border-right: 1px #000 solid;border-bottom: 1px #000 solid;text-align: start;">
+                        <div class="dataFont" style="text-align: start;"><span class="headerFont">Destination:</span>
+                        {{$organization['mQuote']['magaya__Destination']}}</div>
+                    </th>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
 
 @endif
 
--- resources/views/tab_widget/partials/organization.blade.php
+++ resources/views/tab_widget/partials/organization.blade.php
@@ -5,7 +5,7 @@
             <table width="90%" cellspacing="0px" cellpadding="0px" style="border: none; text-align: right; margin-right:5%;">
                 <tr>
                     <th rowspan="2">
-                        <img class="headerIMG" src="{{ url('/images/logo2.png', $extra = [], $secure = 1) }}" alt="Logo" height="100" width="100" style="text-align: center;">
+                       <!-- <img class="headerIMG" src="{{ url('images/pdfv1/magaya.png') }}" alt="Logo" height="100" width="100" style="text-align: center;">-->
                     </th>
                     <th class="session-first" style="float: left; font-size: 28px; vertical-align:top">
                         {{$organization['orgData']['company_name']}}
@@ -18,25 +18,25 @@
             <table id="header" width="100%" cellspacing="0px" cellpadding="0px" style="border: none; text-align: right; vertical-align:top;clear: right;">
                 <tr>
                     <td colspan="12"class="headerFont">
-                        <span><img src="{{ url('images/pdfv1/language.png', $extra = [], $secure = 1) }}" alt="Language" width="24px" height="24px"/>                            
+                        <span><img src="{{ url('images/pdfv1/language.png') }}" alt="Language" width="24px" height="24px"/>                            
                         </span>{{$organization['orgData']['website']}}
                     </td>
                 </tr>
                 <tr>
                     <td colspan="12" class="headerFont">
-                        <span><img src="{{ url('images/pdfv1/phone.png', $extra = [], $secure = 1) }}" alt="Phone" width="24px" height="24px"/>
+                        <span><img src="{{ url('images/pdfv1/phone.png') }}" alt="Phone" width="24px" height="24px"/>
                         </span>{{$organization['orgData']['phone']}}
                     </td>
                 </tr>
                 <tr>
                     <td colspan="12" class="headerFont">
-                        <span><img src="{{ url('images/pdfv1/email.png', $extra = [], $secure = 1) }}" alt="Email" width="24px" height="24px"/>
+                        <span><img src="{{ url('images/pdfv1/email.png') }}" alt="Email" width="24px" height="24px"/>
                         </span>{{$organization['orgData']['primary_email']}}
                     </td>
                 </tr>
                 <tr>
                     <td colspan="12" class="headerFont">
-                        <span><img src="{{ url('images/pdfv1/home.png', $extra = [], $secure = 1) }}" alt="Home" width="24px" height="24px"/>
+                        <span><img src="{{ url('images/pdfv1/home.png') }}" alt="Home" width="24px" height="24px"/>
                         </span>{{$organization['orgData']['country']}}, {{$organization['orgData']['city']}}, {{$organization['orgData']['state']}}
                     </td>
                 </tr>
