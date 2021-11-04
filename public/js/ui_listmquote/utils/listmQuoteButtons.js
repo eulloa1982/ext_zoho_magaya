0e332e3876c5fca2ca30cb4661ae540a22b643f6
brugo87 <83411488+brugo87@users.noreply.github.com>
2021-11-04 16:01:42 -0400

    reparing

--- public/js/ui_listmquote/utils/listmQuoteButtons.js
+++ public/js/ui_listmquote/utils/listmQuoteButtons.js
@@ -201,7 +201,6 @@ $(document).ready(function(){
 
                 if (k.code !== "SUCCESS") {
                     codeError = k.code;
-                    console.log('name ' + k.details.api_name)
                     field = k.details.api_name;
                     show = true;
                     module = 'Cargo Items'
