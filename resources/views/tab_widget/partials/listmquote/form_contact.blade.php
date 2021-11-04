78f4dfcbe87a35256a3156e5276a808e51293179
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-11-03 23:55:46 -0600

    EStilizando modal New Contact

--- resources/views/tab_widget/partials/listmquote/form_contact.blade.php
+++ resources/views/tab_widget/partials/listmquote/form_contact.blade.php
@@ -3,20 +3,23 @@
 	<div class="modal-dialog">
 		<div class="modal-content">
 
-			<div class="modal-header" style="background: #0b3355;color: white;">
+			<div class="modal-header" style="background: #006494;color: white;">
 				<h4 class="modal-title custom_align" style="font-weight: bold;" id="Title">Add Contact</h4><span class="arrows-quote" class="float-right"></span>
 			    <span class="material-icons btn btn-primary" data-dismiss="modal">close</span>
 			</div>
 			<div class="modal-body">
                 <form id="contact_form">
                     <div class="container" >
-                        <div class="row">
+                        <div class="w-25 p-1 bg-light">Account Name</div>
+                        <div class="row shadow p-3 border border-secondary mb-2 bg-white rounded">
                             <div class="col-md-12">
                                 <label class="col-md-12" style="font-weight: bold;">Account</label>
                                 <select name="Account_Name" class="form-control" ></select>
                             </div>
                         </div>
-                        <div class="row" style="margin-top:20px">
+
+                        <div class="w-25 p-1 bg-light">Contact Name</div>
+                        <div class="row shadow p-3 border border-secondary mb-2 bg-white rounded">
                             <div class="col-md-6">
                                 <label class="col-md-12" style="font-weight: bold;">First Name</label>
                                 <input type="text" name='First_Name' class='form-control' />
@@ -26,30 +29,33 @@
                                 <input type="text" name='Last_Name' class='form-control' />
                             </div>
                         </div>
-                        <div class="row" style="margin-bottom:20px">
+
+                        <div class="w-25 p-1 bg-light">Contact Data</div>
+                        <div class="row shadow p-3 border border-secondary mt-10 mb-2 bg-white rounded" style="margin-bottom:20px">
                             <div class="col-md-4">
-                                <label class="col-md-12" style="font-weight: bold;">Email</label>
+                                <label class="col-md-12" style="font-weight: bold;"><span class="material-icons">alternate_email</span>Email</label>
                                 <input type="text" name='Email' class='form-control' />
                             </div>
-                            <div class="col-md-4">
-                                <label class="col-md-12" style="font-weight: bold;">Phone</label>
+                            <div class="col-md-2">
+                                <label class="col-md-12" style="font-weight: bold;"><span class="material-icons">phone</span>Personal</label>
                                 <input type="text" name='Phone' class='form-control' />
                             </div>
-                            <div class="col-md-4">
-                                <label class="col-md-12" style="font-weight: bold;">Home Phone</label>
+                            <div class="col-md-2">
+                                <label class="col-md-12" style="font-weight: bold;"><span class="material-icons">phone</span>Home</label>
                                 <input type="text" name='Home_Phone' class='form-control' />
                             </div>
-                            <div class="col-md-4">
-                                <label class="col-md-12" style="font-weight: bold;">Mobile</label>
+                            <div class="col-md-2">
+                                <label class="col-md-12" style="font-weight: bold;"><span class="material-icons">phone_iphone</span>Mobile</label>
                                 <input type="text" name='Mobile' class='form-control' />
                             </div>
-                            <div class="col-md-4">
-                                <label class="col-md-12" style="font-weight: bold;">Fax</label>
+                            <div class="col-md-2">
+                                <label class="col-md-12" style="font-weight: bold;"><span class="material-icons">fax</span>Fax</label>
                                 <input type="text" name='Fax' class='form-control' />
                             </div>
                         </div>
 
-                        <div class="row" style="margin-top:35px">
+                        <div class="w-25 p-1 bg-light">Mailing Address</div>
+                        <div class="row shadow p-3 border border-secondary mb-2 bg-white rounded">
                             <div class="col-md-4">
                                 <label class="col-md-12" style="font-weight: bold;">Mailing City</label>
                                 <input type="text" name='Mailing_City' class='form-control' />
@@ -72,7 +78,8 @@
                             </div>
                         </div>
 
-                        <div class="row" style="margin-top:35px">
+                        <div class="w-25 p-1 bg-light">Other Address</div>
+                        <div class="p-3 row border border-secondary bg-white rounded">
                             <div class="col-md-4">
                                 <label class="col-md-12" style="font-weight: bold;">Other City</label>
                                 <input type="text" name='Other_City' class='form-control' />
