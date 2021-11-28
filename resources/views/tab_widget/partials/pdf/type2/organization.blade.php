dd92eb14d0a7ebddadc2ca1dc6997c53cacbae12
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2021-11-27 19:47:02 -0600

    PDF type 2

--- resources/views/tab_widget/partials/pdf/type2/organization.blade.php
+++ resources/views/tab_widget/partials/pdf/type2/organization.blade.php
@@ -3,7 +3,7 @@
     @if (@isset($organization['orgData']['company_name']))
         <span style="float: right; font-size: 28px; vertical-align:top;">{{$organization['orgData']['company_name']}}</span><br />
     @endif
-    <div style="float: left; font-size: 14px; display: block; margin-top:30px">
+    <div style="width: 260px; padding: 5px 5px 5px 5px; float: left; font-size: 14px; display: block; margin-top:30px; border: 1px solid brown;"><strong>Org Info</strong><br />
         @if (@isset($organization['orgData']['website']))
             <span><img src="{{ url('images/pdfv1/language.png') }}" alt="Language" width="24px" height="24px"/></span>
             {{$organization['orgData']['website']}}
@@ -25,26 +25,64 @@
         @if (@isset($organization['orgData']['city'])) {{$organization['orgData']['city']}}  , @endif
         @if (@isset($organization['orgData']['state'])) {{$organization['orgData']['state']}} <br /> @endif
 
-</div>
-<div style="float: left; font-size: 14px; display: block; margin-left: 120px; margin-top:30px">
-    @if (@isset($organization['mQuote']['Name']))
-        <strong>Name: </strong>{{$organization['mQuote']['Name']}} <br />
-    @endif
+    </div>
+    <div style="width: 260px; padding: 5px 5px 5px 5px; float: left; font-size: 14px; display: block; margin-left: 120px; margin-top:30px; border: 1px solid brown;"><strong>Quote Info</strong><br />
+        @if (@isset($organization['mQuote']['Name']))
+            <strong>Name: </strong>{{$organization['mQuote']['Name']}} <br />
+        @endif
 
-    @if (@isset($organization['mQuote']['Created_Time']))
-        <strong>Creation Date:</strong> <?php echo explode('T',$organization['mQuote']['Created_Time'])[0]; ?>
+        @if (@isset($organization['mQuote']['Created_Time']))
+            <strong>Creation Date:</strong> <?php echo explode('T',$organization['mQuote']['Created_Time'])[0]; ?>
+            <br />
+        @endif
+
+        @if ( @isset($organization['mQuote']['magaya__ExpirationDate']) )
+            <strong>Expiration Date:</strong> <?php echo explode('T',$organization['mQuote']['magaya__ExpirationDate'])[0]; ?>
+            <br />
+        @endif
+
+        @if (@isset($organization['mQuote']['Owner']['name']))
+            <strong>Contact To:</strong> {{$organization['mQuote']['Owner']['name']}},<br> {{$organization['mQuote']['Owner']['email']}}
+        @endif
+    </div>
+    <br /><br /><br /><br /><br /><br /><br />
+
+    <div style="width: 93%; padding: 5px 5px 5px 5px; font-size: 14px; border: 1px solid blue; margin-top:30px"><strong>Customer Info</strong><br />
+        @if (@isset($organization['mQuote']['Account']))
+        <strong>Customer:</strong> {{$organization['mQuote']['Account']['name']}}
         <br />
-    @endif
+        @endif
 
-    @if ( @isset($organization['mQuote']['magaya__ExpirationDate']) )
-        <strong>Expiration Date:</strong> <?php echo explode('T',$organization['mQuote']['magaya__ExpirationDate'])[0]; ?>
-        <br />
-    @endif
+        @if (@isset($organization['mQuote']['magaya__Representative']))
+            <strong>Representative: </strong>{{$organization['mQuote']['magaya__Representative']['name']}}
+            <br />
+        @endif
 
-    @if (@isset($organization['mQuote']['Owner']['name']))
-        <strong>Contact To:</strong> {{$organization['mQuote']['Owner']['name']}},<br> {{$organization['mQuote']['Owner']['email']}}
-    @endif
-</div>
-<br /><br /><br /><br /><br /><br /><br />
+        @if (@isset($organization['mQuote']['magaya__ContactMobile']))
+            <strong>Mobile:</strong> {{$organization['mQuote']['magaya__ContactMobile']}}
+            <br />
+        @endif
+
+        @if (@isset($organization['mQuote']['magaya__ContactEmail']))
+            <strong>Email:</strong> {{$organization['mQuote']['magaya__ContactEmail']}}
+            <br />
+        @endif
+
+        <strong>Address:</strong>
+        @if (@isset($organization['mQuote']['magaya__BillingStreet']))
+            {{$organization['mQuote']['magaya__BillingStreet']}} ,
+        @endif
+        @if (@isset($organization['mQuote']['magaya__BillingCity']))
+            {{$organization['mQuote']['magaya__BillingCity']}} ,
+        @endif
+        @if (@isset($organization['mQuote']['magaya__BillingState']))
+            {{$organization['mQuote']['magaya__BillingState']}} , 
+        @endif
+        @if (@isset($organization['mQuote']['magaya__BillingCountry']))
+            {{$organization['mQuote']['magaya__BillingCountry']}}
+        @endif
+    </div>
+    <br /><br />
+
 @endif
 
---
+++ resources/views/tab_widget/partials/pdf/type2/terms.blade.php
@@ -0,0 +1,33 @@
+<table class="info" cellspacing="0px" cellpadding="0px" width="100%" style="margin-top: 10px">
+    <tr>
+        <th colspan="12" style="padding: 5px 0px 5px 5px; background-color: #577BC1; text-align: start;text-transform:uppercase ;border: 1px #000 solid">TERMS & CONDITIONS</th>
+    </tr>
+    @if (@isset($organization['mQuote']['magaya__Terms']))
+    <tr>
+        <td colspan="12" style="border-left: 1px #000 solid;border-right: 1px #000 solid;border-bottom: 1px #000 solid; text-align: left;">{{$organization['mQuote']['magaya__Terms']}}</td>
+    </tr>
+    @else
+    <tr>
+        <td colspan="12" style="border-left: 1px #000 solid;border-right: 1px #000 solid;border-bottom: 1px #000 solid; text-align: left;"><br></td>
+    </tr>
+    @endif
+</table>
+
+<div style="opacity: 0.5; width: 260px; padding: 5px 5px 5px 5px; float: left; font-size: 14px; display: block; margin-top:30px;"><strong>Org Info</strong><br />
+
+    @if (@isset($organization['mQuote']['magaya__Description']))
+        <strong>Description: </strong>{{$organization['mQuote']['magaya__Description']}}
+        <br />
+    @endif
+
+    @if (@isset($organization['mQuote']['magaya__Origin']))
+        <strong>Origin: </strong>{{$organization['mQuote']['magaya__Origin']}}
+        <br />
+    @endif
+
+    @if (@isset($organization['mQuote']['magaya__Destination']))
+        <strong>Destination: </strong>{{$organization['mQuote']['magaya__Destination']}}
+        <br />
+    @endif
+
+</div>
