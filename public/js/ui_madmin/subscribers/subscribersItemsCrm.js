1e0e125a4a6fbd06be672b918720ac19200dd23b
eulloa1982 <65827708+eulloa1982@users.noreply.github.com>
2022-01-16 17:13:18 -0600

    Fixing select bug
    En un select con opciones true - false, el valor false no se pintaba correctamente

--- public/js/ui_madmin/subscribers/subscribersItemsCrm.js
+++ public/js/ui_madmin/subscribers/subscribersItemsCrm.js
@@ -33,23 +33,23 @@ storeCrm.subscribe(() => {
             if ( _.has(ITEMS_CRM, v)) {
 
                 if (!k)
-                    k = false
+                    k = ""
                 let field = _.get(ITEMS_CRM, [v, 'field'])
                 let values = _.has(ITEMS_CRM, [v, "values"]) ? _.get(ITEMS_CRM, [v, 'values']) : ''
                 input = `<input type="text" class="form-control" name="${v}" value="${k}"/>`
-                //append += `<span class="btn btn-primary float-right">Save</span><br /><br />`
 
                 if (!_.isEmpty(values)) {
 
                     input = `<select name="${v}" class="form-control">`
-                        $.map(values, function(val) {
-                            if (val === k)
-                                input += `<option value="${val}" selected>${val}</option>`
-                            else
-                                input += `<option value="${val}">${val}</option>`
-                        })
+                    $.map(values, function(val) {
+                        if (val === k)
+                            input += `<option value="${val}" selected>${val}</option>`
+                        else
+                            input += `<option value="${val}">${val}</option>`
+                    })
                     input += `</select>`
                 }
+
                 append += `<div class="row" style="margin: 5px 5px 5px 5px">
                     <div class="col-md-4" style="font-weight: bold; padding: 5px 5px 5px 5px">${field}</div>
                     <div class="col-md-6" style="font-weight: bold; padding: 5px 5px 5px 5px">${input}</div>
